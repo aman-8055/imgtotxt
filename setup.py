4d3101684e5d539ff6d6522d13d5931744b78413
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-06-03 12:10:06 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='image_captioning',
+    version='1.0',
+    description='Streamlit app for image captioning',
+    author='Your Name',
+    author_email='your_email@example.com',
+    packages=[''],
+    install_requires=[
+        'streamlit',
+        'transformers',
+        'Pillow',
+    ],
+)
